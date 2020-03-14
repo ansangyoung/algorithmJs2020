808dd4e72c1be452142ec9829e02a4a3c549203a
ssffwert12 <ssffwert12@gmail.com>
2020-03-15 08:45:41 +0900

    2 x n 타일링

---
+++ prob12900.js
@@ -0,0 +1,11 @@
+//https://programmers.co.kr/learn/courses/30/lessons/12900
+
+function solution(n){
+    var tiling = [0, 1, 2];
+    for(var i=3; i<=n; i++) {
+    	tiling[i] = (tiling[i-1] + tiling[i-2]) % 1000000007;
+    }
+    return tiling[n];
+}
+var n = 4;
+console.log(solution(n));
