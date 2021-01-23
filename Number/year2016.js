ce10cfad8c865ad314115f3d7c17846fadc0ddff
ssffwert <ssffwert@naver.com>
2021-01-23 16:55:22 +0900

    폴더 이동

--- Number/year2016.js
+++ Number/year2016.js
@@ -1,7 +1,6 @@
 // https://programmers.co.kr/learn/courses/30/lessons/12901
 var a = 5, b = 24;
-//solution(a, b);
-console.log('solution:' + solution(a, b));
+console.log(solution(a, b));
 function solution(a, b) {
     var answer = year2016(a, b);
     return answer;
@@ -10,9 +9,7 @@ function solution(a, b) {
 function year2016(a, b) {
     var dayOfTheWeek = ['SUN', 'MON', 'TUE', 'WED', 'THU', 'FRI', 'SAT'];
     var s = '';
-    for (val of s) {
-    	console.log('test');
-    }
+    
     if(a < 10) {
     	a = '0' + a;
     }
@@ -20,9 +17,6 @@ function year2016(a, b) {
     	b = '0' + b;
     }
     var transDate = '2016-' + a + '-' + b;
-    //console.log('transDate:' + transDate);
     var returnDay = dayOfTheWeek[new Date(transDate).getDay()];
-    //console.log(new Date(transDate).getDay());
-    //console.log('returnDay:' + returnDay);
     return returnDay;
 }
