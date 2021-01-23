0ea5a9bac32c8881be4ccfaccbca296f3a17887f
ssffwert <ssffwert@naver.com>
2021-01-23 16:57:52 +0900

    폴더 이동

--- String/prob12916.js
+++ String/prob12916.js
@@ -1,18 +1,19 @@
 // https://programmers.co.kr/learn/courses/30/lessons/12916
 
-function solution(s){
+function solution(s) {
     var answer = pWithYNum(s);
     return answer;
 }
 
 function pWithYNum(s) {
-	let pNum = 0, yNum = 0;
+	let pNum = 0
+	let yNum = 0;
 	s = s.toLowerCase();
 	for(var val of s) {
 		if(val == 'p') {
-			pNum = pNum+1;
+			pNum = pNum + 1;
 		} else if(val == 'y') {
-			yNum = yNum+1;
+			yNum = yNum + 1;
 		}
 	}
 	let numCompareWithFlg = false;
@@ -21,6 +22,7 @@ function pWithYNum(s) {
 	}
 	return numCompareWithFlg;
 }
+
 s = 'pPoooyY';
 console.log(solution(s));
 s = '-Pyy';
