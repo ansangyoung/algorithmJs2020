7b585d73b55889c82bf989e11cbfcd14e45cd245
ssffwert <ssffwert@naver.com>
2021-01-23 16:50:02 +0900

    폴더 이동

--- DynamicProgramming/prob42585.js
+++ DynamicProgramming/prob42585.js
@@ -1,20 +1,23 @@
-//https://programmers.co.kr/learn/courses/30/lessons/42585
+// https://programmers.co.kr/learn/courses/30/lessons/42585
 
-function solution(arrangement){
-    var stickCurrentCnt = 0, stickAccumulateResultCnt = 0;
+function solution(arrangement) {
     var laserLen = arrangement.length;
-    for(var i=0; i<laserLen; i++){
-    	if(arrangement[i] == '('){
+    var stickCurrentCnt = 0
+    var stickAccumulateResultCnt = 0;
+    
+    for(var i = 0; i < laserLen; i++){
+    	if(arrangement[i] == '(') {
     		stickCurrentCnt++;
     	} else{
     		stickCurrentCnt--;
-    		if(arrangement[i-1] == ')'){
+    		if(arrangement[i-1] == ')') {
     			stickAccumulateResultCnt += 1;
     		} else{
     			stickAccumulateResultCnt += stickCurrentCnt;
     		}
     	}
     }
+    
     return stickAccumulateResultCnt;
 }
 
