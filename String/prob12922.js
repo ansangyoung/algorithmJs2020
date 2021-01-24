0e9d5ddd989bdd09e2c87a7e655a70f30ce19ab0
ssffwert <ssffwert@naver.com>
2021-01-24 17:01:09 +0900

    폴더 이동

--- String/prob12922.js
+++ String/prob12922.js
@@ -1,16 +1,16 @@
 // https://programmers.co.kr/learn/courses/30/lessons/12922
 
-function solution(n){
+function solution(n) {
     var answer = waterMelon(n);
     return answer;
 }
 
 function waterMelon(n) {
 	let waterMelonStr = '';
-	for(var i=0; i<n; i=i+2) {
+	for(var i = 0; i < n; i = i + 2) {
 		waterMelonStr += '수박';
 	}
-	if(n%2 == 1) {
+	if(n % 2 == 1) {
 		waterMelonStr = waterMelonStr.substring(0, n);
 	}
 	return waterMelonStr;
